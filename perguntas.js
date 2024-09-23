b956f8dece499de06d9b3aec47199ef72aac984b
annica12 <161027780+annica12@users.noreply.github.com>
2024-09-23 08:46:55 -0300

    23/09/2024 aulas: 4-5-6-7

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    '1)',
+    'O que Chico Moedas faz da vida?',
+    'Ele é investidor de bitcoin (BTC) e outras criptomoedas'
+)
+
+criaCartao(
+    '2)',
+    'Qual é o canal do Chico Moedas?',
+    'O programa se chama Aqueles Cara'
+)
+
+criaCartao(
+    '3)',
+    'Como foi a traição de Chico Moedas?',
+    'Chico traiu Luísa Sonza com outra mulher no banheiro de um bar do Rio de Janeiro'
+)
